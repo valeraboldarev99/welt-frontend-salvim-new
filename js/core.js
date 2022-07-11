f75917ab2d9b584ed9e1eb1822d3e6b6f88a8b68
Valera Boldarev <vboldarev@weltkind.com>
2022-07-11 13:59:00 +0600

    main page

---
+++ js/core.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+	// слaйдер на главной
+	$('.slider').slick({
+		slidesToScroll:1,
+		slidesToShow:1,
+		// autoplay: true,
+		// appendArrows:'.slider-arrows',
+		autoplaySpeed: 3000,
+		pauseOnHover:true,
+		arrows:false,
+		dots:true,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					arrows:false,
+				}
+			}
+		]
+	});
+
+	/*мобильное меню*/
+	var mobileSidebar = function()
+	{
+		function obj()
+		{
+			var self = this;
+			self.animSpeed = 400;
+			self.init = function()
+			{
+				self.events();
+			},
+			self.events = function()
+			{
+				$('.sidebar-open').click(function() {
+					self.open();
+				});
+				$('.sidebar-close, .sidebar-overlay').click(function() {
+					self.close();
+				});
+			},
+			self.open = function()
+			{
+				$('.sidebar').addClass('sidebar_opened');
+				$('.sidebar-overlay').fadeIn(self.animSpeed);
+				// pageScroll.hide(1);
+			},
+			self.close = function()
+			{
+				$('.sidebar').removeClass('sidebar_opened');
+				$('.sidebar-overlay').fadeOut(self.animSpeed);
+				// pageScroll.show(0);
+			}
+		}
+		return new obj();
+	}();
+	mobileSidebar.init();
+});
